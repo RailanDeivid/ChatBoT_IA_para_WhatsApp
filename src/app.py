d7027b362b4a4407fc3644c834228512906e52ee
Railan Deivid <contato.railandeivid@gmail.com>
2025-08-12 18:21:46 -0300

    update

--- src/app.py
+++ src/app.py
@@ -38,53 +38,20 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 
-load_dotenv()
+from src.config import (
+   STRING_CONEXAO,OPENAI_API_KEY,AI_SYSTEM_PROMPT,OPENAI_MODEL_NAME
+)
 
-usuario = os.getenv("DB_USER")
-senha = os.getenv("DB_PASSWORD")
-porta = os.getenv("DB_PORT")
-nome_do_banco = os.getenv("DB_NAME")
-host = os.getenv("DB_HOST")
-api_key = os.getenv("OPENAI_API_KEY")
-os.environ['OPENAI_API_KEY'] = api_key
 
-string_conexao = f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{nome_do_banco}"
-
-model = ChatOpenAI(model='gpt-4-turbo')
-db = SQLDatabase.from_uri(string_conexao)
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+model = ChatOpenAI(OPENAI_MODEL_NAME)
+db = SQLDatabase.from_uri(STRING_CONEXAO)
 toolkit = SQLDatabaseToolkit(db=db, llm=model)
 system_message = hub.pull('hwchase17/react')
 
 agent = create_react_agent(llm=model, tools=toolkit.get_tools(), prompt=system_message)
 agent_executor = AgentExecutor(agent=agent, tools=toolkit.get_tools(), verbose=True)
 
-prompt_template = PromptTemplate.from_template("""
-Você é um especialista em compras de produtos para bares e restaurantes.
-
-Sua tarefa é responder perguntas utilizando **apenas** as informações contidas na tabela `505 compra`.  
-Não utilize nenhuma informação externa.
-
-**Referências da tabela:**
-- Período: use a coluna `D. Lançamento`.
-- Nome da casa: coluna `FANTASIA`.
-- Valores monetários: coluna `V. Contábil`.
-- Quantidades: coluna `Q. Estoque`.
-- Grupo de produtos: coluna `Grande Grupo`.
-- Subgrupo de produtos: coluna `Subgrupo`.
-- Nome dos produtos: coluna `Descrição Item`.
-
-**Regras para análise:**
-1. Para cálculos de valores, use apenas a coluna `V. Contábil`.
-2. Para cálculos de quantidade, use apenas a coluna `Q. Estoque`.
-3. Para análises por período, utilize exclusivamente a coluna `D. Lançamento`.
-4. Caso a pergunta não possa ser respondida por falta de dados:
-   - Se o problema for na coluna `FANTASIA`, retorne apenas as opções disponíveis dessa coluna.
-   - Se o problema for na coluna `Grande Grupo`, retorne apenas as opções disponíveis dessa coluna.
-   - Se o problema for na coluna `Subgrupo`, retorne apenas as opções disponíveis dessa coluna.
-5. Responda sempre em português brasileiro e seja o mais preciso possível.
-
-Pergunta: {q}
-""")
 
 app = FastAPI()
 
@@ -111,7 +78,7 @@ async def webhook(request: Request):
         else:
             # Já respondeu a saudação, processa a pergunta no agente
             try:
-                prompt = prompt_template.format(q=message)
+                prompt = AI_SYSTEM_PROMPT.format(q=message)
                 output = agent_executor.invoke({"input": prompt})
                 resposta = output.get('output', 'Desculpe, não consegui responder sua pergunta no momento.')
                 send_watsapp_message(
